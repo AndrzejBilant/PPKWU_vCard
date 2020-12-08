7a6750261704e699c54f1984da0dea8a2d3556b1
Bilant.Andrzej <215679@edu.p.lodz.pl>
2020-12-08 15:47:40 +0100

    added html bootstrap skeleton to print records, not working yet

--- src/main/java/PPKWU/PPKWU_vCard/DataCollector.java
+++ src/main/java/PPKWU/PPKWU_vCard/DataCollector.java
@@ -19,6 +19,69 @@ import java.util.ArrayList;
     @RequestMapping(path = "/company")
     public String getCompaniesListFormPANORAMAFIRM(@RequestParam(value = "name") String name, HttpServletResponse response) {
         StringBuilder stringBuilder = new StringBuilder();
+//        stringBuilder.append("<html>\n"
+//                             + "<head>\n"
+//                             + "    <title>Table Data Addition</title>\n"
+//                             + "    <meta charset=\"utf-8\">\n"
+//                             + "    <link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css\">\n"
+//                             + "    <link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.10.1/bootstrap-table.min.css\">\n"
+//                             + "    <script src=\"https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.4/jquery.min.js\"></script>\n"
+//                             + "    <script src=\"https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js\"></script>\n"
+//                             + "    <script src=\"https://cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.10.1/bootstrap-table.min.js\"></script>\n"
+//                             + "</head>\n"
+//                             + "<body>\n"
+//                             + "    <div class=\"container\">\n"
+//                             + "        <table id=\"table\" data-height=\"460\">\n"
+//                             + "        <thead>\n"
+//                             + "            <tr>\n"
+//                             + "                <th data-field=\"name\">name</th>\n"
+//                             + "                <th data-field=\"telephone\">telephone</th>\n"
+//                             + "                <th data-field=\"email\">email</th>\n"
+//                             + "                <th data-field=\"sameAs\">sameAs</th>\n"
+//                             + "                <th data-field=\"address\">address</th>\n"
+//                             + "            </tr>\n"
+//                             + "        </thead>\n"
+//                             + "    </table>\n"
+//                             + "    </div>\n"
+//                             +" <script type=\"text/javascript\"> \n"
+//                             + "    $(document).ready(function () { \n"
+//                             + "  \n"
+//                             + "      $('table').bootstrapTable({ \n"
+//                             + "        data: mydata \n"
+//                             + "      }); \n"
+//                             + "    }); \n"
+//                             + "  \n"
+//                             + "    var mydata = \n"
+//                             + "      [ \n"
+//                             + "        { \n"
+//                             + "          \"id\": \"24323\", \n"
+//                             + "          \"name\": \"Mark Smith\", \n"
+//                             + "          \"date\": \"25/5/2020\" \n"
+//                             + "        }, \n"
+//                             + "        { \n"
+//                             + "          \"id\": \"24564\", \n"
+//                             + "          \"name\": \"Caitlin MacDonald\", \n"
+//                             + "          \"date\": \"17/5/2020\" \n"
+//                             + "        }, \n"
+//                             + "        { \n"
+//                             + "          \"id\": \"24345\", \n"
+//                             + "          \"name\": \"Jessie Johnson \", \n"
+//                             + "          \"date\": \"1/5/2020\" \n"
+//                             + "        }, \n"
+//                             + "        { \n"
+//                             + "          \"id\": \"24874\", \n"
+//                             + "          \"name\": \"Alen Williams\", \n"
+//                             + "          \"date\": \"14/5/2020\" \n"
+//                             + "        }, \n"
+//                             + "        { \n"
+//                             + "          \"id\": \"24323\", \n"
+//                             + "          \"name\": \"Maria Garcia \", \n"
+//                             + "          \"date\": \"13/5/2020\" \n"
+//                             + "        } \n"
+//                             + "      ]; \n"
+//                             + "  </script> "
+//                             + "</body>\n"
+//                             + "</html>");
         String url = "https://panoramafirm.pl/szukaj?k=";
         String localization = "&l=";
         System.out.println(name);
@@ -39,7 +102,7 @@ import java.util.ArrayList;
             }
         }
         for (Company company : companies) {
-            stringBuilder.append(companies + "<br>");
+            stringBuilder.append(company + "<br>");
         }
         return stringBuilder.toString();
     }
@@ -53,19 +116,20 @@ import java.util.ArrayList;
         Address address;
 
         @Override public String toString() {
-            return "Company{"
+            return "Company{<br>"
                    + "name='"
                    + name
                    + "<br>"
-                   + ", telephone='"
+                   + "telephone="
                    + telephone
                    + "<br>"
-                   + ", email='"
+                   + "email="
                    + email
                    + "<br>"
-                   + ", sameAs="
+                   + "sameAs="
                    + sameAs
-                   + ", address="
+                   + "<br>"
+                   + "address="
                    + address
                    + "}<br>";
         }
@@ -80,16 +144,16 @@ import java.util.ArrayList;
 
         @Override public String toString() {
             return "Address{"
-                   + "streetAddress='"
+                   + "streetAddress="
                    + streetAddress
                    + "<br>"
-                   + ", addressLocality='"
+                   + "addressLocality="
                    + addressLocality
                    + "<br>"
-                   + ", postalCode='"
+                   + "postalCode="
                    + postalCode
                    + "<br>"
-                   + ", addressCountry='"
+                   + "addressCountry="
                    + addressCountry
                    + "<br>"
                    + '}';
