e3530136101b6eda6e053f50d33b14d6d6f7073b
Bilant.Andrzej <215679@edu.p.lodz.pl>
2020-12-08 15:23:16 +0100

    added creation of objects from filtered data

--- src/main/java/PPKWU/PPKWU_vCard/DataCollector.java
+++ src/main/java/PPKWU/PPKWU_vCard/DataCollector.java
@@ -1,15 +1,19 @@
 package PPKWU.PPKWU_vCard;
 
+import com.google.gson.Gson;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.springframework.boot.json.GsonJsonParser;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
 
 @RestController public class DataCollector {
 
@@ -27,12 +31,67 @@ import java.io.IOException;
         } catch (IOException ioException) {
             ioException.printStackTrace();
         }
+        Gson gson = new Gson();
+        ArrayList<Company> companies = new ArrayList<>();
         Elements elements = document.select("script");
         for (Element element : elements) {
             if (element.attr("type").equals("application/ld+json")) {
-                System.out.println(element.data());
-
+                companies.add(gson.fromJson(element.data(),Company.class));
             }
+        }
+        for (Company company : companies) {
+            System.out.println(company);
+        }
+
+    }
+
+    class Company{
+        String name;
+        String telephone;
+        String email;
+        URL sameAs;
+        Address address;
+
+        @Override public String toString() {
+            return "Company{"
+                   + "name='"
+                   + name
+                   + '\''
+                   + ", telephone='"
+                   + telephone
+                   + '\''
+                   + ", email='"
+                   + email
+                   + '\''
+                   + ", sameAs="
+                   + sameAs
+                   + ", address="
+                   + address
+                   + '}';
+        }
+    }
+
+    class Address{
+        String streetAddress;
+        String addressLocality;
+        String postalCode;
+        String addressCountry;
+
+        @Override public String toString() {
+            return "Address{"
+                   + "streetAddress='"
+                   + streetAddress
+                   + '\''
+                   + ", addressLocality='"
+                   + addressLocality
+                   + '\''
+                   + ", postalCode='"
+                   + postalCode
+                   + '\''
+                   + ", addressCountry='"
+                   + addressCountry
+                   + '\''
+                   + '}';
         }
     }
 }
