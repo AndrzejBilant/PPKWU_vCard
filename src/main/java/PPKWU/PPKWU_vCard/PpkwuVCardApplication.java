ab5232b64bd7773c36170156d7193e9b733d3d36
Bilant.Andrzej <215679@edu.p.lodz.pl>
2020-12-08 14:32:07 +0100

    Generated skeleton from Spring Boot Initializer

---
+++ src/main/java/PPKWU/PPKWU_vCard/PpkwuVCardApplication.java
@@ -0,0 +1,13 @@
+package PPKWU.PPKWU_vCard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpkwuVCardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpkwuVCardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/PPKWU/PPKWU_vCard/PpkwuVCardApplicationTests.java
@@ -0,0 +1,13 @@
+package PPKWU.PPKWU_vCard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpkwuVCardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
